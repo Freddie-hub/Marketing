0201c1c54e6f48861e67b81493391fbe0f343565
Igadwa Magomere <83275851+Brian-Magomere-Igadwa@users.noreply.github.com>
2024-04-16 07:21:32 +0300

    Solves #4 - Ensure logout button appears in the same state in all other pages navigated to from the navbar.

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,88 +1,137 @@
-import React, { useState } from 'react';
-import Navbar from '../components/navbar';
-
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/navbar";
 
 function ContactSection() {
+  const [authToken, setAuthToken] = useState("");
+  const handleLogOut = () => {
+    localStorage.removeItem("auth_token");
+    // setAuthToken("");
+    window.location.reload();
+  };
+  useEffect(() => {
+    // Check if the token exists in localStorage
+    const authenticatedToken = localStorage.getItem("auth_token");
+    if (authenticatedToken) setAuthToken(authenticatedToken);
+  }, []);
   return (
     <div>
-    <Navbar/>
-    <section className="section contact bg-white-2" id="contact" aria-label="contact">
-      <div className="container">
+      <Navbar authToken={authToken} handleLogOut={handleLogOut} />
+      <section
+        className="section contact bg-white-2"
+        id="contact"
+        aria-label="contact"
+      >
+        <div className="container">
+          <h2 className="h2 section-title text-center text-3xl font-bold mt-7">
+            Connect With Us
+          </h2>
 
-        <h2 className="h2 section-title text-center text-3xl font-bold mt-7">Connect With Us</h2>
+          <form
+            action=""
+            className="contact-form bg-white-1 p-20 rounded-radius-2 mb-30 shadow-shadow-1"
+          >
+            <div className="input-wrapper">
+              <input
+                type="text"
+                name="name"
+                aria-label="name"
+                placeholder="Your name*"
+                required
+                className="input-field bg-white-2 text-eerie-black text-fs-5 p-15 rounded-radius-2 outline-none mb-15"
+              />
 
-        <form action="" className="contact-form bg-white-1 p-20 rounded-radius-2 mb-30 shadow-shadow-1">
-
-          <div className="input-wrapper">
-            <input type="text" name="name" aria-label="name" placeholder="Your name*" required className="input-field bg-white-2 text-eerie-black text-fs-5 p-15 rounded-radius-2 outline-none mb-15" />
-
-            <input type="email" name="email_address" aria-label="email" placeholder="Email address*" required className="input-field" />
-          </div>
-
-          <div className="input-wrapper">
-            <input type="text" name="subject" aria-label="subject" placeholder="Subject" className="input-field" />
-
-            <input type="number" name="phone" aria-label="phone" placeholder="Phone number" className="input-field" />
-          </div>
-
-          <textarea name="message" aria-label="message" placeholder="Your message...*" required className="input-field"></textarea>
-
-          <div className="checkbox-wrapper">
-            <input type="checkbox" name="terms_and_policy" id="terms" required className="checkbox" />
-
-          </div>
-
-          <button type="submit" className="btn btn-primary">Send Message</button>
-
-        </form>
-
-        <ul className="contact-list">
-
-          <li className="contact-item">
-            <div className="contact-card">
-
-              <div className="card-icon">
-                <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
-              </div>
-
-              <div className="card-content">
-
-                <h3 className="h3 card-title">Mail Here</h3>
-
-                <a href="mailto:hello@luaz.com" className="card-link">hello@luaz.com</a>
-                <a href="mailto:info@luaz.com" className="card-link">info@luaz.com</a>
-
-              </div>
-
+              <input
+                type="email"
+                name="email_address"
+                aria-label="email"
+                placeholder="Email address*"
+                required
+                className="input-field"
+              />
             </div>
-          </li>
 
-          <li className="contact-item">
-            <div className="contact-card">
+            <div className="input-wrapper">
+              <input
+                type="text"
+                name="subject"
+                aria-label="subject"
+                placeholder="Subject"
+                className="input-field"
+              />
 
-              <div className="card-icon">
-                <ion-icon name="map-outline" aria-hidden="true"></ion-icon>
-              </div>
-
-             
-
+              <input
+                type="number"
+                name="phone"
+                aria-label="phone"
+                placeholder="Phone number"
+                className="input-field"
+              />
             </div>
-          </li>
 
-          <li className="contact-item">
-            <div className="contact-card">
+            <textarea
+              name="message"
+              aria-label="message"
+              placeholder="Your message...*"
+              required
+              className="input-field"
+            ></textarea>
 
-              <div className="card-icon">
-                <ion-icon name="headset-outline" aria-hidden="true"></ion-icon>
-              </div>
-
+            <div className="checkbox-wrapper">
+              <input
+                type="checkbox"
+                name="terms_and_policy"
+                id="terms"
+                required
+                className="checkbox"
+              />
             </div>
-          </li>
 
-        </ul>
+            <button type="submit" className="btn btn-primary">
+              Send Message
+            </button>
+          </form>
 
-      </div>
-    </section>
+          <ul className="contact-list">
+            <li className="contact-item">
+              <div className="contact-card">
+                <div className="card-icon">
+                  <ion-icon name="mail-outline" aria-hidden="true"></ion-icon>
+                </div>
+
+                <div className="card-content">
+                  <h3 className="h3 card-title">Mail Here</h3>
+
+                  <a href="mailto:hello@luaz.com" className="card-link">
+                    hello@luaz.com
+                  </a>
+                  <a href="mailto:info@luaz.com" className="card-link">
+                    info@luaz.com
+                  </a>
+                </div>
+              </div>
+            </li>
+
+            <li className="contact-item">
+              <div className="contact-card">
+                <div className="card-icon">
+                  <ion-icon name="map-outline" aria-hidden="true"></ion-icon>
+                </div>
+              </div>
+            </li>
+
+            <li className="contact-item">
+              <div className="contact-card">
+                <div className="card-icon">
+                  <ion-icon
+                    name="headset-outline"
+                    aria-hidden="true"
+                  ></ion-icon>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
--- frontend/src/pages/projectpage.jsx
+++ frontend/src/pages/projectpage.jsx
@@ -1,108 +1,179 @@
-import React, { useState } from 'react';
-import project1Image from '../assets/images/project-1.jpg';
-import project2Image from '../assets/images/project-2.jpg';
-import project3Image from '../assets/images/project-3.jpg';
-import project4Image from '../assets/images/project-4.jpg';
-import project5Image from '../assets/images/project-5.jpg';
-import project6Image from '../assets/images/project-6.jpg';
-import Navbar from '../components/navbar';
+import React, { useEffect,useState } from "react";
+import project1Image from "../assets/images/project-1.jpg";
+import project2Image from "../assets/images/project-2.jpg";
+import project3Image from "../assets/images/project-3.jpg";
+import project4Image from "../assets/images/project-4.jpg";
+import project5Image from "../assets/images/project-5.jpg";
+import project6Image from "../assets/images/project-6.jpg";
+import Navbar from "../components/navbar";
 
-const ProjectsPage = () => {    
-    return (
-        <div>
-            <Navbar/>
-            <section className="section project" id="project" aria-label="project">
-                <div className="container">
-                    <h2 className="text-center text-3xl font-bold mt-7">Our Recent Projects</h2>
-                    <p className="section-text"></p>
-                    <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-7 ml-4 mr-4 mb-10">
-                        <li>
-                            <div className="project-card relative rounded-lg overflow-hidden">
-                                <figure className="card-banner img-holder">
-                                    <img src={project1Image} loading="lazy" alt="Designing a better cinema experience" className="object-cover w-full h-full" />
-                                </figure>
-                                <div className="card-content">
-                                    <p className="text-lavender-gray text-base leading-tight">SEO Optimization</p>
-                                    <h3 className="text-black">
-                                        <a href="#" className="text-black">Designing a better cinema experience</a>
-                                    </h3>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="project-card relative rounded-lg overflow-hidden">
-                                <figure className="card-banner img-holder">
-                                    <img src={project2Image} loading="lazy" alt="Designing a better cinema experience" className="object-cover w-full h-full" />
-                                </figure>
-                                <div className="card-content">
-                                    <p className="text-lavender-gray text-base leading-tight">SEO Optimization</p>
-                                    <h3 className="text-black">
-                                        <a href="#" className="text-black">Designing a better cinema experience</a>
-                                    </h3>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="project-card relative rounded-lg overflow-hidden">
-                                <figure className="card-banner img-holder">
-                                    <img src={project3Image} loading="lazy" alt="Designing a better cinema experience" className="object-cover w-full h-full" />
-                                </figure>
-                                <div className="card-content">
-                                    <p className="text-lavender-gray text-base leading-tight">SEO Optimization</p>
-                                    <h3 className="text-black">
-                                        <a href="#" className="text-black">Designing a better cinema experience</a>
-                                    </h3>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="project-card relative rounded-lg overflow-hidden">
-                                <figure className="card-banner img-holder">
-                                    <img src={project4Image} loading="lazy" alt="Designing a better cinema experience" className="object-cover w-full h-full" />
-                                </figure>
-                                <div className="card-content">
-                                    <p className="text-lavender-gray text-base leading-tight">SEO Optimization</p>
-                                    <h3 className="text-black">
-                                        <a href="#" className="text-black">Designing a better cinema experience</a>
-                                    </h3>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="project-card relative rounded-lg overflow-hidden">
-                                <figure className="card-banner img-holder">
-                                    <img src={project5Image} loading="lazy" alt="Designing a better cinema experience" className="object-cover w-full h-full" />
-                                </figure>
-                                <div className="card-content">
-                                    <p className="text-lavender-gray text-base leading-tight">SEO Optimization</p>
-                                    <h3 className="text-black">
-                                        <a href="#" className="text-black">Designing a better cinema experience</a>
-                                    </h3>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="project-card relative rounded-lg overflow-hidden">
-                                <figure className="card-banner img-holder">
-                                    <img src={project6Image} loading="lazy" alt="Designing a better cinema experience" className="object-cover w-full h-full" />
-                                </figure>
-                                <div className="card-content">
-                                    <p className="text-lavender-gray text-base leading-tight">SEO Optimization</p>
-                                    <h3 className="text-black">
-                                        <a href="#" className="text-black">Designing a better cinema experience</a>
-                                    </h3>
-                                </div>
-                            </div>
-                        </li>
-                        {/* Add more project cards here */}
-                    </ul>
-                    <footer className="bg-gray-800 text-white text-center py-4">
-                       <p>&copy; {new Date().getFullYear()} R and J Group. All rights reserved.</p>
-                    </footer>
+const ProjectsPage = () => {
+  const [authToken, setAuthToken] = useState("");
+  const handleLogOut = () => {
+    localStorage.removeItem("auth_token");
+    // setAuthToken("");
+    window.location.reload();
+  };
+  useEffect(() => {
+    // Check if the token exists in localStorage
+    const authenticatedToken = localStorage.getItem("auth_token");
+    if (authenticatedToken) setAuthToken(authenticatedToken);
+  }, []);
+
+  return (
+    <div>
+      <Navbar authToken={authToken} handleLogOut={handleLogOut} />
+      <section className="section project" id="project" aria-label="project">
+        <div className="container">
+          <h2 className="text-center text-3xl font-bold mt-7">
+            Our Recent Projects
+          </h2>
+          <p className="section-text"></p>
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-7 ml-4 mr-4 mb-10">
+            <li>
+              <div className="project-card relative rounded-lg overflow-hidden">
+                <figure className="card-banner img-holder">
+                  <img
+                    src={project1Image}
+                    loading="lazy"
+                    alt="Designing a better cinema experience"
+                    className="object-cover w-full h-full"
+                  />
+                </figure>
+                <div className="card-content">
+                  <p className="text-lavender-gray text-base leading-tight">
+                    SEO Optimization
+                  </p>
+                  <h3 className="text-black">
+                    <a href="#" className="text-black">
+                      Designing a better cinema experience
+                    </a>
+                  </h3>
                 </div>
-            </section>
+              </div>
+            </li>
+            <li>
+              <div className="project-card relative rounded-lg overflow-hidden">
+                <figure className="card-banner img-holder">
+                  <img
+                    src={project2Image}
+                    loading="lazy"
+                    alt="Designing a better cinema experience"
+                    className="object-cover w-full h-full"
+                  />
+                </figure>
+                <div className="card-content">
+                  <p className="text-lavender-gray text-base leading-tight">
+                    SEO Optimization
+                  </p>
+                  <h3 className="text-black">
+                    <a href="#" className="text-black">
+                      Designing a better cinema experience
+                    </a>
+                  </h3>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="project-card relative rounded-lg overflow-hidden">
+                <figure className="card-banner img-holder">
+                  <img
+                    src={project3Image}
+                    loading="lazy"
+                    alt="Designing a better cinema experience"
+                    className="object-cover w-full h-full"
+                  />
+                </figure>
+                <div className="card-content">
+                  <p className="text-lavender-gray text-base leading-tight">
+                    SEO Optimization
+                  </p>
+                  <h3 className="text-black">
+                    <a href="#" className="text-black">
+                      Designing a better cinema experience
+                    </a>
+                  </h3>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="project-card relative rounded-lg overflow-hidden">
+                <figure className="card-banner img-holder">
+                  <img
+                    src={project4Image}
+                    loading="lazy"
+                    alt="Designing a better cinema experience"
+                    className="object-cover w-full h-full"
+                  />
+                </figure>
+                <div className="card-content">
+                  <p className="text-lavender-gray text-base leading-tight">
+                    SEO Optimization
+                  </p>
+                  <h3 className="text-black">
+                    <a href="#" className="text-black">
+                      Designing a better cinema experience
+                    </a>
+                  </h3>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="project-card relative rounded-lg overflow-hidden">
+                <figure className="card-banner img-holder">
+                  <img
+                    src={project5Image}
+                    loading="lazy"
+                    alt="Designing a better cinema experience"
+                    className="object-cover w-full h-full"
+                  />
+                </figure>
+                <div className="card-content">
+                  <p className="text-lavender-gray text-base leading-tight">
+                    SEO Optimization
+                  </p>
+                  <h3 className="text-black">
+                    <a href="#" className="text-black">
+                      Designing a better cinema experience
+                    </a>
+                  </h3>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="project-card relative rounded-lg overflow-hidden">
+                <figure className="card-banner img-holder">
+                  <img
+                    src={project6Image}
+                    loading="lazy"
+                    alt="Designing a better cinema experience"
+                    className="object-cover w-full h-full"
+                  />
+                </figure>
+                <div className="card-content">
+                  <p className="text-lavender-gray text-base leading-tight">
+                    SEO Optimization
+                  </p>
+                  <h3 className="text-black">
+                    <a href="#" className="text-black">
+                      Designing a better cinema experience
+                    </a>
+                  </h3>
+                </div>
+              </div>
+            </li>
+            {/* Add more project cards here */}
+          </ul>
+          <footer className="bg-gray-800 text-white text-center py-4">
+            <p>
+              &copy; {new Date().getFullYear()} R and J Group. All rights
+              reserved.
+            </p>
+          </footer>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default ProjectsPage;
